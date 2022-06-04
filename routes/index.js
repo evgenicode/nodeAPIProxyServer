3af26ac6df443dfd01ec7c7452229c041ce937e7
evgenii <evgenii.contact@gmail.com>
2022-06-04 07:16:07 +0300

    forwarding query params

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
 
+// Environment variables
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
 router.get('/', async (inRequest, inResponse) => {
-  const apiResonse = await needle('get', )
+  try {
+    console.log()
+
+    const params = new URLSearchParams({
+      [API_KEY_NAME] : API_KEY_VALUE,
+      ...url.parse(inRequest.url, true).query
+    })
+    //console.log(params)
+    const apiResponse = await needle('get', `${API_BASE_URL}?${params}`)
+    const data = apiResponse.body
+
+    // Log the request to the public API
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
+
+    inResponse.status(200).json(data)
+  } catch (error) {
+    inResponse.status(500).json({ error })
+  }
+
 })
 
 module.exports = router
